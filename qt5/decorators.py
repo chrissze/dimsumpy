bbc2fa8c3bdc7ecfbd08f3b0f62885eb1e26c4b3
Chris <3chris@gmail.com>
2020-06-08 17:49:45 +0800

    macpro

---
+++ qt5/decorators.py
@@ -0,0 +1,10 @@
+from PySide2.QtWidgets import QMessageBox
+
+
+def confirmation_box(func):
+    def wrapper(*args):
+        reply: QMessageBox.StandardButton = QMessageBox.question(
+            args[0], 'Confirmation', 'Action?', QMessageBox.Yes | QMessageBox.Cancel)
+        if reply == QMessageBox.Yes:
+            func()
+    return wrapper
