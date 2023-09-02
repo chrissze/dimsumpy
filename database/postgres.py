223ad1954eecf2f4515eeb10a868804ba8ad2418
Chris Sze <8147803+gate-max@users.noreply.github.com>
2023-09-02 14:52:06 +0800

    pro

--- database/postgres.py
+++ database/postgres.py
@@ -3,32 +3,44 @@ from typing import Any, Dict, List
 from pandas import DataFrame
 from psycopg2.extensions import connection, cursor
 
-def upsertquery(tablename: str, cols: List[str], keys: List[str]) -> str:
-    cols_str = ', '.join(cols)
-    l = len(cols)
-    marks = ', '.join(['%s'] * l)
-    keys_str = ', '.join(keys)
-    cols_nonkey = [x for x in cols if x not in keys]
-    cols_nonkey_str = ', '.join(cols_nonkey)
-    update_str = ', '.join(list(map(lambda s: 'EXCLUDED.' + s, cols_nonkey)))
-    query_str = ''.join(['INSERT INTO ', tablename, ' (', cols_str, ') VALUES (', marks
-                        , ') ON CONFLICT (', keys_str, ') DO UPDATE SET (', cols_nonkey_str
-                        , ') = (', update_str, ')'])
+def make_psycopg_upsert_query(table_name: str, columns: List[str], primary_key_list: List[str]) -> str:
+    '''
+    * INDEPENDENT *
+
+    make a psycopg version 3 Postgres UPSERT SQL string without semicolon.
+    return SQL query example:
+        INSERT INTO stock_guru (symbol, td, t, wealth_pc) VALUES (%s, %s, %s, %s) ON CONFLICT (symbol, td) DO UPDATE SET (t, wealth_pc) = (EXCLUDED.t, EXCLUDED.wealth_pc)
+    '''
+    columns_str: str = ', '.join(columns)
+    number_of_columns: int = len(columns)
+    placeholders: str = ', '.join(['%s'] * number_of_columns)
+    primary_key_str: str = ', '.join(primary_key_list)
+    non_primary_key_columns: List[str] = [x for x in columns if x not in primary_key_list]
+    non_primary_key_columns_str: str = ', '.join(non_primary_key_columns)
+    update_part: str = ', '.join(list(map(lambda column: f'EXCLUDED.{column}', non_primary_key_columns)))
+    query_str: str = f'INSERT INTO {table_name} ({columns_str}) VALUES ({placeholders}) ON CONFLICT ({primary_key_str}) DO UPDATE SET ({non_primary_key_columns_str}) = ({update_part})'
     return query_str
 
 
-def upsert_dict(table: str, dict: Dict, primarykeys: List[str], con: connection) -> str:
-    """ I should place symbol as first element of keys, so that it can be returned"""
+
+
+def psycopg_upsert(dict: Dict, table: str, primary_key_list: List[str], con: connection) -> str:
+    '''
+    Get the first value(symbol) of the primary_key_list by
+        symbol: Any = dict.get(primary_key_list[0])
+    
+    I should place symbol as first element of keys, so that it can be returned
+    '''
     try:
         c: cursor = con.cursor()
-        query: str = upsertquery(table, dict.keys(), primarykeys)
+        query: str = make_psycopg_upsert_query(table, dict.keys(), primary_key_list)
         values = tuple(dict.values())
         print(f'query is {query}')
         c.execute(query, values)
         c.close()
         con.commit()
-        pkvalue: Any = dict.get(primarykeys[0])
-        return str(pkvalue) # should be the symbol
+        pkvalue: Any = dict.get(primary_key_list[0])
+        return str(pkvalue) # s
     except Exception as err:
         print(err)
         return str(err)
@@ -49,3 +61,8 @@ def execute_postgres_command(command: str, con: connection ) -> str:
         print(err)
         return str(err)
 
+if __name__ == '__main__':
+    q1 = make_psycopg_upsert('stock_guru', ['symbol', 'td', 't', 'wealth_pc'], ['symbol', 'td'])
+
+    print('q1 is: ', q1)
+    print()
