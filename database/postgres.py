2e1b256d3118632d838d1816cf23719fbd0d8587
Chris Sze <8147803+gate-max@users.noreply.github.com>
2023-09-02 21:09:30 +0800

    pro

--- database/postgres.py
+++ database/postgres.py
@@ -1,9 +1,45 @@
-from typing import Any, Dict, List
+'''
+I should put this into requirements.txt to install - psycopg[binary, pool]
 
-from pandas import DataFrame
-from psycopg2.extensions import connection, cursor
+psycopg Documentation:
+https://www.psycopg.org/psycopg3/docs/
 
-def make_psycopg_upsert_query(table_name: str, columns: List[str], primary_key_list: List[str]) -> str:
+executemany:
+    CORRECT:    cur.executemany(cmd, [('aa.com',),('bb.com',)])
+    WRONG:    cur.executemany(cmd, ['aa.com', 'bb.com')
+
+    executemany does not accept list() function as a sequence parameter        
+    CORRECT: domain_list = list(my_generator) 
+            cur.executemany(cmd, domain_list)
+    WRONG: cur.executemany(cmd, list(my_generator))
+
+
+'''
+
+
+from typing import Any, Dict, List, Tuple
+
+from psycopg import Connection  # psycopg 3
+
+
+
+def execute_psycopg(command: str, connection: Connection) -> None:
+    '''
+    * INDEPENDENT *
+    IMPORTS: psycopg
+
+    the connection parameter can be a function that returns a psycopg 3 Connection. For example, make_psycopg_connection() in pizzapy program.
+
+    con.commit() will just return None for successful execution.
+    I can use pandas.read_sql if I want to fetch the result.
+    '''
+    with connection as con:
+        con.execute(command)
+        con.commit()
+    
+
+
+def make_upsert_psycopg_query(table: str, columns: List[str], primary_key_list: List[str]) -> str:
     '''
     * INDEPENDENT *
 
@@ -18,51 +54,60 @@ def make_psycopg_upsert_query(table_name: str, columns: List[str], primary_key_l
     non_primary_key_columns: List[str] = [x for x in columns if x not in primary_key_list]
     non_primary_key_columns_str: str = ', '.join(non_primary_key_columns)
     update_part: str = ', '.join(list(map(lambda column: f'EXCLUDED.{column}', non_primary_key_columns)))
-    query_str: str = f'INSERT INTO {table_name} ({columns_str}) VALUES ({placeholders}) ON CONFLICT ({primary_key_str}) DO UPDATE SET ({non_primary_key_columns_str}) = ({update_part})'
+    query_str: str = f'INSERT INTO {table} ({columns_str}) VALUES ({placeholders}) ON CONFLICT ({primary_key_str}) DO UPDATE SET ({non_primary_key_columns_str}) = ({update_part})'
     return query_str
 
 
 
+def upsert_psycopg(dict: Dict, table: str, primary_key_list: List[str], connection: Connection) -> str:
+    '''
+    DEPENDS ON:  make_upsert_psycopg_query()
+    IMPORTS: psycopg
 
-def psycopg_upsert(dict: Dict, table: str, primary_key_list: List[str], con: connection) -> str:
+    the connection parameter can be a function that returns a psycopg 3 Connection. For example, make_psycopg_connection() in pizzapy program.
     '''
-    Get the first value(symbol) of the primary_key_list by
-        symbol: Any = dict.get(primary_key_list[0])
-    
-    I should place symbol as first element of keys, so that it can be returned
-    '''
-    try:
-        c: cursor = con.cursor()
-        query: str = make_psycopg_upsert_query(table, dict.keys(), primary_key_list)
-        values = tuple(dict.values())
-        print(f'query is {query}')
-        c.execute(query, values)
-        c.close()
+    query: str = make_upsert_psycopg_query(table, columns=dict.keys(), primary_key_list=primary_key_list)
+    values = tuple(dict.values())
+    with connection as con:
+        con.execute(query, values)
         con.commit()
-        pkvalue: Any = dict.get(primary_key_list[0])
-        return str(pkvalue) # s
-    except Exception as err:
-        print(err)
-        return str(err)
+    query_and_values: str = f'{query} {values}'
+    return query_and_values
 
 
 
-def execute_postgres_command(command: str, con: connection ) -> str:
+def upsert_many_psycopg(dictionaries: List[Dict], table: str, primary_key_list: List[str], connection: Connection) -> str:
     '''
-    There might be similar function in the official psycopg
+    DEPENDS ON:  make_upsert_psycopg_query()
+    IMPORTS: psycopg
+
+    dictionaries argument is a list of ProxyDict in pizzapy.
+
+    dict.values() return a dictionary object, I have to convert it to tuple in order to be used in psycopg.
+
+    the connection parameter can be a function that returns a psycopg 3 Connection. For example, make_psycopg_connection() in pizzapy program.
+
+    this function is tested successfully on pizzapy/reference_example/executemany_success.py
+
+    However, it is not practical to fetch mutiple DictProxies and accumulate them, then run this function. As this increase the code complexity and harder to debug. Not much time saved.
+
+    I would rather loop through a list of stock symbols, fetch a DictProxy, and run upsert_psycopg() one by one.
+
     '''
-    try:
-        c: cursor = con.cursor()
-        c.execute(command)
-        c.close()
-        result = con.commit()
-        return str(result)
-    except Exception as err:
-        print(err)
-        return str(err)
+    columns: List[str] = dictionaries[0].keys() if dictionaries else []
+    query: str = make_upsert_psycopg_query(table, columns=columns, primary_key_list=primary_key_list)
+    values_list: List[List] = [dict.values() for dict in dictionaries]
+    
+    with connection as con:
+        with con.cursor() as cur:
+            cur.executemany(query, values_list)
+        con.commit()
+    return query
+
+
 
 if __name__ == '__main__':
-    q1 = make_psycopg_upsert('stock_guru', ['symbol', 'td', 't', 'wealth_pc'], ['symbol', 'td'])
+    q1 = make_upsert_psycopg_query('stock_guru', ['symbol', 'td', 't', 'wealth_pc'], ['symbol', 'td'])
 
     print('q1 is: ', q1)
     print()
