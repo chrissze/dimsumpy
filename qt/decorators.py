f428fc6bd0f9c4900b13719c0af8b2c9167ffc3e
CS <you@example.com>
2023-09-16 14:12:46 +0800

    pro

--- qt/decorators.py
+++ qt/decorators.py
@@ -10,4 +10,19 @@ def confirmation_self(method):
             method_output = method(self, *method_args, **method_kwargs)
             return method_output
     return _impl
-    
+
+
+
+def list_confirmation():
+    def decorator(method):
+        @wraps(method)
+        def _impl(self, xs):
+            number: int = len(xs)
+            reply: QMessageBox.StandardButton = QMessageBox.question(
+                self, 'Confirmation', f'Do you want to work on {number} items?', QMessageBox.Yes | QMessageBox.Cancel)
+            if reply == QMessageBox.Yes:
+                method_output = method(self, xs)
+                return method_output
+        return _impl
+    return decorator
+
