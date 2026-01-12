00dea89fcd9045b57b2d02e0146b5ac4c4af4b0e
macbook13 <3chris@gmail.com>
2026-01-12 11:45:11 +0800

    minor update

--- dimsumpy/av.py
+++ dimsumpy/av.py
@@ -6,6 +6,12 @@ DOCS:
 
     
 NOTES:
+
+- my USD 50 plan is 5 limited at 5 requests per second
+
+- httpx.AsyncClient()'s default timeout is 5 seconds.
+
+
 async def main() -> None:
     x1 = await gets_overview('AMD')
     x2 = await gets_overview('NVDA')
@@ -23,6 +29,7 @@ async def main() -> None:
 
 import asyncio
 
+from datetime import date, datetime
 from pprint import pprint
 import os
 
@@ -31,6 +38,108 @@ import os
 
 
 import httpx
+
+from httpx import Response
+
+
+
+def get_etf_profile(symbol: str, apikey=None) -> dict[str, str | list[dict[str, str]]]:
+
+    if apikey is None:
+        apikey = os.getenv('AV_API_KEY')
+        
+    params: dict[str, str] = {'function': 'ETF_PROFILE', 'symbol': symbol, 'apikey': apikey}
+    
+    r: Response = httpx.get('https://www.alphavantage.co/query', params=params)
+    
+    r.raise_for_status()
+    
+    data: dict[str, str | list[dict[str, str]]] = r.json()
+    
+    return data
+
+
+
+async def async_etf_profile(symbol: str, apikey=None) -> dict[str, str | list[dict[str, str]]]:
+    if apikey is None:
+        apikey = os.getenv("AV_API_KEY")
+
+    params: dict[str, str] = {'function': 'ETF_PROFILE', 'symbol': symbol, 'apikey': apikey}
+
+    async with httpx.AsyncClient() as client:
+        r = await client.get('https://www.alphavantage.co/query', params=params)
+        r.raise_for_status()
+        data: dict[str, str | list[dict[str, str]]] = r.json()
+
+    return data
+
+
+
+
+def get_historical_options(symbol: str, isodate=None, datatype='json', apikey=None) -> dict[str, str | list[dict[str, str]]]:
+    """
+    # https://www.alphavantage.co/documentation/#historical-options
+    """
+    if apikey is None:
+        apikey = os.getenv('AV_API_KEY')
+        
+    params: dict[str, str] = {
+        'function': 'HISTORICAL_OPTIONS', 
+        'symbol': symbol, 
+        'datatype': datatype,
+        'apikey': apikey,
+        }
+    
+    if isinstance(isodate, str):
+        params['date'] = isodate 
+    
+    if isinstance(isodate, date):
+        isodate = isodate.isoformat()
+        params['date'] = isodate 
+    
+    if isinstance(isodate, datetime):
+        isodate = isodate.date().isoformat()
+        params['date'] = isodate 
+    
+    r: Response = httpx.get('https://www.alphavantage.co/query', params=params)
+    
+    data: dict[str, str | list[dict[str, str]]] = r.json()
+    
+    return data
+
+
+
+# todo
+def async_historical_options(symbol: str, isodate=None, datatype='json', apikey=None) -> dict[str, str | list[dict[str, str]]]:
+    """
+    # https://www.alphavantage.co/documentation/#historical-options
+    """
+    if apikey is None:
+        apikey = os.getenv('AV_API_KEY')
+        
+    params: dict[str, str] = {
+        'function': 'HISTORICAL_OPTIONS', 
+        'symbol': symbol, 
+        'datatype': datatype,
+        'apikey': apikey,
+        }
+    
+    if isinstance(isodate, str):
+        params['date'] = isodate 
+    
+    if isinstance(isodate, date):
+        isodate = isodate.isoformat()
+        params['date'] = isodate 
+    
+    if isinstance(isodate, datetime):
+        isodate = isodate.date().isoformat()
+        params['date'] = isodate 
+    
+    r: Response = httpx.get('https://www.alphavantage.co/query', params=params)
+    
+    data: dict[str, str | list[dict[str, str]]] = r.json()
+    
+    return data
 
 
 
@@ -42,22 +151,26 @@ def get_overview(symbol: str, apikey=None) -> dict[str, str]:
         
     params: dict[str, str] = {'function': 'OVERVIEW', 'symbol': symbol, 'apikey': apikey}
     
-    data: dict[str, str] = httpx.get('https://www.alphavantage.co/query', params=params).json()
+    r: Response = httpx.get('https://www.alphavantage.co/query', params=params)
+
+    r.raise_for_status()
+    
+    data: dict[str, str] = r.json()
     
     return data
 
 
 
-async def gets_overview(symbol: str, apikey=None) -> dict[str, str]:
+async def async_overview(symbol: str, apikey=None) -> dict[str, str]:
     if apikey is None:
         apikey = os.getenv("AV_API_KEY")
 
     params: dict[str, str] = {'function': 'OVERVIEW', 'symbol': symbol, 'apikey': apikey}
 
     async with httpx.AsyncClient() as client:
-        resp = await client.get('https://www.alphavantage.co/query', params=params)
-        resp.raise_for_status()
-        data: dict[str, str] = resp.json()
+        r: Response = await client.get('https://www.alphavantage.co/query', params=params)
+        r.raise_for_status()
+        data: dict[str, str] = r.json()
 
     return data
 
@@ -67,36 +180,8 @@ async def gets_overview(symbol: str, apikey=None) -> dict[str, str]:
 
 
 
-def get_etf_profile(symbol: str, apikey=None) -> dict[str, str | list[dict[str, str]]]:
 
-    if apikey is None:
-        apikey = os.getenv('AV_API_KEY')
-        
-    params: dict[str, str] = {'function': 'ETF_PROFILE', 'symbol': symbol, 'apikey': apikey}
-    
-    data: dict[str, str | list[dict[str, str]]] = httpx.get('https://www.alphavantage.co/query', params=params).json()
-    
-    return data
-
-
-
-async def gets_etf_profile(symbol: str, apikey=None) -> dict[str, str | list[dict[str, str]]]:
-    if apikey is None:
-        apikey = os.getenv("AV_API_KEY")
-
-    params: dict[str, str] = {'function': 'ETF_PROFILE', 'symbol': symbol, 'apikey': apikey}
-
-    async with httpx.AsyncClient() as client:
-        resp = await client.get('https://www.alphavantage.co/query', params=params)
-        resp.raise_for_status()
-        data: dict[str, str | list[dict[str, str]]] = resp.json()
-
-    return data
-
-
-
-
-def get_time_series_daily(symbol: str, apikey=None, outputsize='compact', datatype='json') -> dict[str, dict[str, str] | dict[str, dict[str, dict]]]:
+def get_time_series_daily(symbol: str, outputsize='compact', datatype='json', apikey=None) -> dict[str, dict[str, str] | dict[str, dict[str, str]]]:
 
     if apikey is None:
         apikey = os.getenv('AV_API_KEY')
@@ -104,23 +189,50 @@ def get_time_series_daily(symbol: str, apikey=None, outputsize='compact', dataty
     params: dict[str, str] = {
         'function': 'TIME_SERIES_DAILY', 
         'symbol': symbol, 
-        'apikey': apikey,
         'outputsize': outputsize,
         'datatype': datatype,
+        'apikey': apikey,
         }
     
-    data: dict[str, str] = httpx.get('https://www.alphavantage.co/query', params=params).json()
+    r: Response = httpx.get('https://www.alphavantage.co/query', params=params)
+    
+    data: dict[str, dict[str, str] | dict[str, dict[str, str]]] = r.json()
     
     return data
 
 
+
+
+async def async_time_series_daily(symbol: str, outputsize='compact', datatype='json', apikey=None) -> dict[str, dict[str, str] | dict[str, dict[str, str]]]:
+
+    if apikey is None:
+        apikey = os.getenv('AV_API_KEY')
+        
+    params: dict[str, str] = {
+        'function': 'TIME_SERIES_DAILY', 
+        'symbol': symbol, 
+        'outputsize': outputsize,
+        'datatype': datatype,
+        'apikey': apikey,
+        }
+    
+    async with httpx.AsyncClient() as client:
+        r: Response = await client.get('https://www.alphavantage.co/query', params=params)
+        r.raise_for_status()
+        data: dict[str, dict[str, str] | dict[str, dict[str, str]]] = r.json()
+    
+    return data
+
+
+
+
 async def main() -> None:
     x1, x2, x3, x4, x5 = await asyncio.gather(
-        gets_overview('AMD'),
-        gets_overview('NVDA'),
-        gets_overview('AAPL'),
-        gets_overview('META'),
-        gets_etf_profile('QQQ'),
+        async_overview('AMD'),
+        async_overview('NVDA'),
+        async_overview('AAPL'),
+        async_etf_profile('QQQ'),
+        async_time_series_daily('META'),
     )
 
     print(x1, x2, x3, x4, x5)
@@ -128,6 +240,7 @@ async def main() -> None:
 
 if __name__ == '__main__':
     #asyncio.run(main())
-    pprint(get_time_series_daily('AMD'))
+    #pprint(get_overview('AMD'))
+    pprint(get_historical_options('QQQ'))
     
     
