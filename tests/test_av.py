7d56cd3ebd83114f55b86f4097baf03c8320f4c6
macbook13 <3chris@gmail.com>
2026-01-12 20:22:26 +0800

    minor update

---
+++ tests/test_av.py
@@ -0,0 +1,103 @@
+
+import pytest
+
+from dimsumpy.av import get_cap, async_cap, get_close, async_close, get_etf_aum, async_etf_aum, get_etf_list, async_etf_list
+
+
+def test_get_cap() -> None:
+    c1 = get_cap('AMD')
+    c2 = get_cap('QQQ')
+    c3 = get_cap('XXXXXX')
+    
+    assert isinstance(c1, float)
+    assert c1 > 100_000_000_000.0
+    assert isinstance(c2, float)
+    assert c2 > 100_000_000_000.0
+    assert c3 is None
+    
+
+@pytest.mark.asyncio
+async def test_async_cap():
+    c1 = await async_cap('NVDA')
+    c2 = await async_cap('SPY')
+    c3 = await async_cap('XXXXXX')
+    
+    assert isinstance(c1, float)
+    assert c1 > 4_000_000_000_000.0
+    assert isinstance(c2, float)
+    assert c2 > 100_000_000_000.0
+    assert c3 is None
+    
+
+def test_get_close() -> None:
+    c1 = get_close('AMD')
+    c2 = get_close('QQQ')
+    c3 = get_close('XXXXXX')
+    
+    assert isinstance(c1, float)
+    assert c1 > 100.0
+    assert isinstance(c2, float)
+    assert c2 > 300.0
+    assert c3 is None
+    
+
+@pytest.mark.asyncio
+async def test_async_close():
+    c1 = await async_close('NVDA')
+    c2 = await async_close('SPY')
+    c3 = await async_close('XXXXXX')
+    
+    assert isinstance(c1, float)
+    assert c1 > 100.0
+    assert isinstance(c2, float)
+    assert c2 > 300.0
+    assert c3 is None
+    
+
+def test_get_etf_aum() -> None:
+    c1 = get_etf_aum('AMD')
+    c2 = get_etf_aum('QQQ')
+    c3 = get_cap('XXXXXX')
+    
+    assert c1 is None
+    
+    assert isinstance(c2, float)
+    assert c2 > 100_000_000_000.0
+    
+    assert c3 is None
+
+
+
+@pytest.mark.asyncio
+async def test_async_etf_aum():
+    c1 = await async_etf_aum('NVDA')
+    c2 = await async_etf_aum('SPY')
+    c3 = await async_etf_aum('XXXXXX')
+    
+    assert c1 is None
+    
+    assert isinstance(c2, float)
+    assert c2 > 100_000_000_000.0
+    
+    assert c3 is None
+
+
+
+def test_get_etf_list() -> None:
+    xs = get_etf_list()
+
+    assert isinstance(xs, list)
+    assert len(xs) > 3000
+    assert 'QQQ' in xs
+    
+    
+
+@pytest.mark.asyncio
+async def test_async_etf_list():
+    xs = await async_etf_list()
+
+    assert isinstance(xs, list)
+    assert len(xs) > 3000
+    assert 'SPY' in xs
+    
+    
